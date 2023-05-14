b673b43d3121ebc60081aa48902ce5912d0a7c65
DELL <DELL@DESKTOP-67BV82A>
2023-05-14 21:45:58 +0700

    fix code dicesum

--- DiceSum.java
+++ DiceSum.java
@@ -19,20 +19,22 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  */
 
-public class DiceSum extends DieRoll {
-    privateDieRoll r1;
-//    private DieRoll r2;
-    public Dice_Sum(DieRoll r1,
-		   DieRoll r2) {
-	super(0,0,0)
-	this.r1=r1;
-	thisr2=r2;
-    }
-    public RollResult makeRoll() {
-	return r1.makeRoll().andThen(r2.makeRoll());
-    }
-    public String toString() {
-	return r1.toString() + " & "+r2.toString();
-    }
-
-}
+	 public class Dice extends DieRoll {
+	    // tách private Dieroll ra cần có khoảng trắng ở giữa
+	    private DieRoll r1;
+	    private DieRoll r2;
+	    
+	    public Dice(DieRoll r1, DieRoll r2) { //tên hàm ko khớp với tên được đặt tên Dice nhưng hàm được tạo tên là Dice_Sum 
+	        super(0, 0, 0);// thiếu dấu ; ở cuối
+	        this.r1 = r1;
+	        this.r2 = r2;// thiếu dấu . ở giữa this và r2
+	    }
+	    
+	    public RollResult makeRoll() {
+	        return r1.makeRoll().andThen(r2.makeRoll());
+	    }
+	    
+	    public String toString() {
+	        return r1.toString() + " & " + r2.toString();
+	    }
+	}
